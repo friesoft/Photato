d03f49024a240af7b0108fc970a0bfd14c2536e7
Bernhard Friedreich <friesoft@gmail.com>
2017-10-14 22:53:46 +0200

    Remove redundand annotations

--- src/main/java/photato/PhotatoApplication.java
+++ src/main/java/photato/PhotatoApplication.java
@@ -2,13 +2,9 @@ package photato;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
-@EnableAutoConfiguration
 public class PhotatoApplication {
 
 	@Autowired

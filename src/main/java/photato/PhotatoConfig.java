c9dc46cdb650231c1ec4690ed572109ac41c5baa
Bernhard Friedreich <friesoft@gmail.com>
2017-10-14 22:56:14 +0200

    removed wrong annotation

--- src/main/java/photato/PhotatoConfig.java
+++ src/main/java/photato/PhotatoConfig.java
@@ -1,12 +1,10 @@
 package photato;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
 
-@EnableConfigurationProperties(PhotatoConfig.class)
 @Configuration
 @ConfigurationProperties(prefix="photato")
 @Data

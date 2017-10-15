f58b483f125c7b35e1e47286987ce34b1d6027a2
Bernhard Friedreich <friesoft@gmail.com>
2017-10-15 21:59:28 +0200

    Added custom spring security login form, temporary loginuser user1:user1Pass

--- src/main/java/photato/WebConfig.java
+++ src/main/java/photato/WebConfig.java
@@ -3,9 +3,11 @@ package photato;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.WebJarsResourceResolver;
 
@@ -20,4 +22,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .resourceChain(true)
                 .addResolver(new WebJarsResourceResolver());
     }
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		WebMvcConfigurer.super.addViewControllers(registry);
+		registry.addViewController("/login").setViewName("login");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	}
 }
